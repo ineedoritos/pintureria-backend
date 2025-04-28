33c14559edefab13482fa5e042159d49bab34492
ineedoritos <cgon991@gmail.com>
2025-04-28 12:21:36 -0600

    hecho el schema de prisma
    sexo

---
+++ src/routes/v1/index.js
@@ -0,0 +1,13 @@
+/*
+src/
+  routes/
+    v1/
+      user.routes.ts
+      product.routes.ts
+    v2/
+      user.routes.ts
+      product.routes.ts
+    v1 es para el versionamiento basicamente, si algun dia se quiere cambiar algo o asi sexo
+*/
+
+      // algo asi xd
